973ec6759815551daa4e229591b145f21fe62469
Oleg Shirokov <oleeq2@gmail.com>
2018-04-09 00:32:45 +0300

    Fix bugs

--- Sources/Converter.cpp
+++ Sources/Converter.cpp
@@ -27,12 +27,22 @@ int main(int argc, char** argv)
 
 	std::unique_ptr<caffe::db::DB> train_db(caffe::db::GetDB("lmdb"));
 	train_db->Open("./cifar10_train_lmdb", caffe::db::NEW);
-	std::unique_ptr<caffe::db::Transaction> train_transaction(train_db->NewTransaction());
 
-	for (int batch_index = 0; batch_index < batches_number; batch_index++)
+	for (int batch_index = 1; batch_index < batches_number + 1; batch_index++)
 	{
+		std::unique_ptr<caffe::db::Transaction> train_transaction(train_db->NewTransaction());
+		if (!train_transaction)
+		{
+			MERROR("Failed to create new train transaction");
+			return -1;
+		}
 		const std::string batch_path = "./data_batch_" + std::to_string(batch_index) + ".bin";
 		auto batch = Batch::FromFile(batch_path);
+		if (!batch)
+		{
+			MERROR("Failed to open batch!");
+			return -1;
+		}
 
 		for (int image_index = 0; image_index < images_per_batch; image_index++)
 		{
@@ -56,15 +66,27 @@ int main(int argc, char** argv)
 			int key = batch_index * images_per_batch + image_index;
 			train_transaction->Put(std::to_string(key), serialized);
 		}
+		INFO("Batch #" << batch_index << " processed");
+		train_transaction->Commit();
 	}
 
-	train_transaction->Commit();
 	train_db->Close();
 
 	std::unique_ptr<caffe::db::DB> test_db(caffe::db::GetDB("lmdb"));
 	test_db->Open("./cifar10_test_lmdb", caffe::db::NEW);
-	std::unique_ptr<caffe::db::Transaction> test_transaction(train_db->NewTransaction());
+	std::unique_ptr<caffe::db::Transaction> test_transaction(test_db->NewTransaction());
+	if (!test_transaction)
+	{
+		MERROR("Failed to create new test transaction");
+		return -1;
+	}
+
 	auto test_batch = Batch::FromFile("./test_batch.bin");
+	if (!test_batch)
+	{
+		MERROR("Failed to open test batch");
+		return -1;
+	}
 	for (int image_index = 0; image_index < images_per_batch; image_index++)
 	{
 		auto image = test_batch->GetNext();
@@ -84,7 +106,10 @@ int main(int argc, char** argv)
 		datum.SerializeToString(&serialized);
 		test_transaction->Put(std::to_string(image_index), serialized);
 	}
+	INFO("Test batch processed");
 
 	test_transaction->Commit();
 	test_db->Close();
+
+	return 0;
 }
